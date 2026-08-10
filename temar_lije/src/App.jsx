12e934c5cc9dc73eabf45ac31621c20effb8025f
Gfani <fanuelgoitom79@gmail.com>
2026-08-10 16:34:27 +0300

    adding Header and footer

--- temar_lije/src/App.jsx
+++ temar_lije/src/App.jsx
@@ -1,13 +1,5 @@
-import CreateAccount from "./features/auth/create_account/create_account"
-import SignIn from "./features/auth/signin/signin"
-import AssignmentsTab from "./features/classroom-detail/tabs/AssignmentsTab/AssignmentsTab"
-import LiveClassTab from "./features/classroom-detail/tabs/LiveClassTab/LiveClassTab"
-import TeacherMemberTab from "./features/classroom-detail/tabs/TeacherMemberTab/TeacherMemberTab"
-import StudyBuddy from "./features/study-buddy/study-buddy"
-import Chat from "./features/chat/chat"
+
 import Header from './components/common/Header/header';
-import Footer from './components/common/Footer/footer';
-import MembersTab from './features/classroom-detail/MembersTab';
 
 
 
@@ -17,15 +9,7 @@ function App() {
   return (
     <>
       <Header />
-      <Chat />
-      <LiveClassTab />
-      <AssignmentsTab />
-      <TeacherMemberTab />
-      <MembersTab/>
-      <StudyBuddy />
-      <SignIn />
-      <CreateAccount />
-      <Footer />
+
     </>
   )
 }

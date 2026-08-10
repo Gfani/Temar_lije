88d4b775f8735c4d98b51b1f884b06a58a4548dc
Gfani <fanuelgoitom79@gmail.com>
2026-08-10 16:42:33 +0300

    fixed

--- temar_lije/src/App.jsx
+++ temar_lije/src/App.jsx
@@ -1,4 +1,12 @@
-
+import CreateAccount from "./features/auth/create_account/create_account"
+import SignIn from "./features/auth/signin/signin"
+import AssignmentsTab from "./features/classroom-detail/tabs/AssignmentsTab/AssignmentsTab"
+import LiveClassTab from "./features/classroom-detail/tabs/LiveClassTab/LiveClassTab"
+import TeacherMemberTab from "./features/classroom-detail/tabs/TeacherMemberTab/TeacherMemberTab"
+import StudyBuddy from "./features/study-buddy/study-buddy"
+import Chat from "./features/chat/chat"
+import Header from './components/common/Header/header';
+import Footer from './components/common/Footer/footer';
 import MembersTab from './features/classroom-detail/MembersTab';
 
 
@@ -8,9 +16,16 @@ import MembersTab from './features/classroom-detail/MembersTab';
 function App() {
   return (
     <>
-
+      <Header />
+      <Chat />
+      <LiveClassTab />
+      <AssignmentsTab />
+      <TeacherMemberTab />
       <MembersTab />
-
+      <StudyBuddy />
+      <SignIn />
+      <CreateAccount />
+      <Footer />
     </>
   )
 }

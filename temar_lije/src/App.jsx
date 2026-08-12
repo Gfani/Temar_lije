ef7c825910918ee5311f9f40bb8da5068671dba3
Gemechu Teso <gemeteso065@gmail.com>
2026-08-12 18:45:22 +0300

    latest updates

--- temar_lije/src/App.jsx
+++ temar_lije/src/App.jsx
@@ -4,17 +4,20 @@ import AssignmentsTab from "./features/classroom-detail/tabs/AssignmentsTab/Assi
 import LiveClassTab from "./features/classroom-detail/tabs/LiveClassTab/LiveClassTab"
 import TeacherMemberTab from "./features/classroom-detail/tabs/TeacherMemberTab/TeacherMemberTab"
 import StudyBuddy from "./features/study-buddy/study-buddy"
+import Title from "./components/common/title/Title" 
 function App() {
 
 
   return (
     <>
+      <Title/>
       <LiveClassTab />
       <AssignmentsTab />
       <TeacherMemberTab />
       <StudyBuddy />
       <SignIn />
       <CreateAccount />
+      
     </>
   )
 }

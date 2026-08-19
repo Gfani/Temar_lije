fa33f24c3ce8ffed48d17e4069a70df8c2472bd2
Gfani <fanuelgoitom79@gmail.com>
2026-08-19 20:27:16 +0300

    fix(backend): safe import for cookie-parser

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser');
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
